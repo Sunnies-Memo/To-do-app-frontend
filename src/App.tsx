501bd945fd99d8b241132fe7bc0148184c4ef409
sunniesfish <hhch5611.jh@gmail.com>
2024-05-14 20:35:47 +0900

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import { Reset } from "./GlobalStyle";
+import { darkTheme } from "./theme";
 
 function App() {
   return (
     <>
+    <ThemeProvider theme={darkTheme}>
+      <Reset/>
+    </ThemeProvider>
     </>
   );
 }
