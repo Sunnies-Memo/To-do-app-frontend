b2d1cd1ffbc13c52f62bda327465e786902f1017
sunniesfish <hhch5611.jh@gmail.com>
2024-05-14 21:09:43 +0900

    #7.1 Set Selectors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import { ThemeProvider } from "styled-components";
 import { Reset } from "./GlobalStyle";
 import { darkTheme } from "./theme";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 
 function App() {
-  return (
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector); // selector 의 반환값 첫번째 arg는 get의 리턴값, 두번째 arg는 set함수
+  const onMinutesChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  }
+  const onHoursChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  }
+    return (
     <>
     <ThemeProvider theme={darkTheme}>
       <Reset/>
+      <input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes"/>
+      <input value={hours} onChange={onHoursChange} type="number" placeholder="Hours"/>
     </ThemeProvider>
     </>
   );
