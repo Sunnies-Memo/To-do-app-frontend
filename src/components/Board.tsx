026bc1f4e62aaaed540d53bd4b66e146c76bc9b7
sunniesfish <hhch5611.jh@gmail.com>
2024-06-10 19:35:57 +0900

    #7.11 Droppable Snapshot

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,13 +3,15 @@ import DragableCard from "./DragableCard";
 import React from "react";
 import { styled } from "styled-components";
 
-const Wrapper = styled.div`
-  width: 250px;
-  background-color: ${props => props.theme.boardColor};
-  padding-top: 30px;
-  padding: 20px 10px;
-  border-radius: 5px;
-  min-height: 200px;
+interface IAreaProps{
+    isDraggingOver:boolean;
+    isDraggingFromThis:boolean;
+}
+
+const Area = styled.div<IAreaProps>`
+    background-color: ${props => props.isDraggingOver? "blue" : props.isDraggingFromThis? "red" : "dimgray"};
+    flex-grow: 1;
+    transition: background-color .3s ease-in-out;
 `
 const Title = styled.div`
     display: flex;
@@ -21,23 +23,40 @@ const Title = styled.div`
         font-weight: bold;
     }
 `
+const Wrapper = styled.div`
+    width: 250px;
+    background-color: ${props => props.theme.boardColor};
+    padding-top: 30px;
+    padding: 20px 10px;
+    border-radius: 5px;
+    min-height: 200px;
+    display: flex;
+    flex-direction: column;
+`
 interface IBoardProps{
     toDos: string[];
     boardId: string;
 }
 function Board({toDos,boardId}:IBoardProps){
     return(
-        <Droppable droppableId={boardId}>
-            {(magic) => 
-            <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
-                <Title><span>{boardId}</span></Title>
-                {toDos.map((todo, index) => 
-                <DragableCard key={todo} index={index} todo={todo}/>
-                )}
-            {magic.placeholder}
-            </Wrapper>
-            }
-        </Droppable>
+        <Wrapper>
+            <Title><span>{boardId}</span></Title>
+            <Droppable droppableId={boardId}>
+                {(magic,snapshot) => 
+                <Area 
+                    ref={magic.innerRef} 
+                    {...magic.droppableProps} 
+                    isDraggingOver={snapshot.isDraggingOver}
+                    isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+                >
+                    {toDos.map((todo, index) => 
+                    <DragableCard key={todo} index={index} todo={todo}/>
+                    )}
+                    {magic.placeholder}
+                </Area>
+                }
+            </Droppable>
+        </Wrapper>
     )
 }
 
